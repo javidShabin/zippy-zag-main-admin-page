7d27b983555ffabbe799310f8868d63e16350b9a
javid shabin <javid.main.act@gmail.com>
2024-08-18 12:17:39 +0530

    Add the route for signup page

--- src/router/routes.jsx
+++ src/router/routes.jsx
@@ -1,9 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
 import AdminLayout from "../layout/AdminLayout";
+import SignupPage from "../page/SignupPage";
 
 export const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <AdminLayout />
-    }
-])
+  {
+    path: "/",
+    element: <AdminLayout />,
+
+    children: [
+      {
+        path: "sign-up",
+        element: <SignupPage />,
+      },
+    ],
+  },
+]);
