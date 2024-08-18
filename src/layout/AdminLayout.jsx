236c0bcd0a872d0a0b0974a2748483f45a84e404
javid shabin <javid.main.act@gmail.com>
2024-08-18 12:20:18 +0530

    aligned the side bar and outlet

--- src/layout/AdminLayout.jsx
+++ src/layout/AdminLayout.jsx
@@ -1,14 +1,19 @@
-import React from 'react'
-import UnAuthSidebar from '../components/UnAuthSidebar'
-import { Outlet } from 'react-router-dom'
+import React from "react";
+import UnAuthSidebar from "../components/UnAuthSidebar";
+import { Outlet } from "react-router-dom";
 
 const AdminLayout = () => {
   return (
-    <div>
-      <UnAuthSidebar />
-      <Outlet />
+    <div className="flex h-screen">
+      <div className="sticky top-0 w-64 bg-white shadow-lg">
+        <UnAuthSidebar />
+      </div>
+      {/* Main Content Section */}
+      <div className="flex-1 bg-gray-100 p-4 overflow-y-auto">
+        <Outlet />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default AdminLayout
+export default AdminLayout;
