f5d29fecf5f8f3ad3e9ab1a1d46cb954da257716
javid shabin <javid.main.act@gmail.com>
2024-12-11 18:11:35 +0530

    make change

--- src/page/Dashbord.jsx
+++ src/page/Dashbord.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const Dashbord = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <section className="flex justify-center items-center gap-7">
+      <div className="w-[350px] h-[200px] bg-[#ff961f] m-6 rounded-lg shadow-md ">
+        <div className="flex justify-between items-center">
+          <h3>Restaurants</h3>
+          <h1 className="text-[150px] font-extrabold text-[#ffffff7c]">21</h1>
+        </div>
+      </div>
+      <div className="w-[350px] h-[200px] bg-[#ffffff] m-6 rounded-lg shadow-md "></div>
+      <div className="w-[350px] h-[200px] bg-[#ffffff] m-6 rounded-lg shadow-md "></div>
+    </section>
+  );
+};
 
-export default Dashbord
+export default Dashbord;
