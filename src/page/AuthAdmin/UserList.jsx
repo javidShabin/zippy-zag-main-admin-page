b389ac662da0934c4b4a825bca8006d45a5824c2
javid shabin <javid.main.act@gmail.com>
2024-12-20 00:26:27 +0530

    defin the trash button

--- src/page/AuthAdmin/UserList.jsx
+++ src/page/AuthAdmin/UserList.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { axiosInstance } from "../../config/axiosInstance";
+import { Trash } from "lucide-react";
 
 const UsersList = () => {
   const [userList, setUserList] = useState([]);
